e6c86bac62f1ae3fb2703d8d5d1a20608c1c8e5e
Гульнара Сахибгареева <gylnaras@hotmail.com>
2018-04-16 18:47:27 +0300

    test1

--- GraduateWorkTest/Program.cs
+++ GraduateWorkTest/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpEntropy;
+using OpenNLP;
 
 namespace GraduateWorkTest
 {
@@ -10,7 +12,15 @@ namespace GraduateWorkTest
     {
         static void Main(string[] args)
         {
-
+            var paragraph = "Mr. & Mrs. Smith is a 2005 American romantic comedy action film. The film stars Brad Pitt and Angelina Jolie as a bored upper-middle class married couple. They are surprised to learn that they are both assassins hired by competing agencies to kill each other.";
+            var modelPath = "path/to/EnglishSD.nbin";
+            var sentenceDetector = EnglishMaximumEntropySentenceDetector(modelPath);
+            var sentences = sentenceDetector.SentenceDetect(paragraph);
+            /* 
+             * sentences = ["Mr. & Mrs. Smith is a 2005 American romantic comedy action film.", 
+             * "The film stars Brad Pitt and Angelina Jolie as a bored upper-middle class married couple.", 
+             * "They are surprised to learn that they are both assassins hired by competing agencies to kill each other."]
+             */
         }
     }
 }
